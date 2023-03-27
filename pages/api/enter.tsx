121cb072cd383f83b6253f1e89b2a8ffb19c02a5
mjjomdev <mjjom.dev@gmail.com>
2023-03-27 22:50:56 +0900

    ADD: handler

--- pages/api/enter.tsx
+++ pages/api/enter.tsx
@@ -1,11 +1,10 @@
+import withHandler from '@/libs/server/withHandler';
 import {NextApiRequest, NextApiResponse} from 'next';
 // import client from '../../../libs/client';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    res.status(401).end();
-  }
-  // req.body.blahblah 형태로 읽어오려면 headers설정해줘야함.
-  console.log(req.body.email);
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.body);
   res.status(200).end();
 }
+
+export default withHandler('POST', handler);
